07fe4872d74d8ea60b5d2f4ef308d2dcff74de83
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2023-04-24 19:46:08 +0530

    Removed the local storage connection string and added a placeholder

--- Azure.Storage.Demo.App/Program.cs
+++ Azure.Storage.Demo.App/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Xml.Linq;
 
-string connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+string connectionString = "<Add_Your_Storage_Connection_String>";
 string containerName = "xml";
 string tableName = "xmloutput";
 string blobName = "Sample.xml";
