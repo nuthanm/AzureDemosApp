12ff0b361b7e02c3dd1915de512c37bb3eaa1d8f
nuthan <inbox.nuthan@gmail.com>
2023-04-24 19:44:01 +0530

    Refactored the code and also data insertion logic changes as per the client request.

--- Azure.Storage.Demo.App/Program.cs
+++ Azure.Storage.Demo.App/Program.cs
@@ -3,13 +3,13 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Specialized;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
-using System.Text.Json;
 using System.Xml.Linq;
 
 string connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
 string containerName = "xml";
 string tableName = "xmloutput";
 string blobName = "Sample.xml";
+string partitionKey = "entityData";
 
 // For blob Storage
 var blobServiceClient = new BlobServiceClient(connectionString);
@@ -30,35 +30,37 @@ var propertyNames = new string[] { "PreferredName", "IndustryCode", "AddressLine
 
 using (Stream stream = await blob.OpenReadAsync())
 {
-    var xmlData = XDocument.Load(stream); 
+    // Read entire xml file as a stream
+    var xmlData = XDocument.Load(stream);
 
-    foreach (var propertyName in propertyNames)
+    // Get the EntityInformation element information
+    var entityInformations = xmlData.Root?.Elements("Body").Descendants("EntityInformation");
+
+    if (entityInformations is null)
     {
-        var elementValues = xmlData.Descendants(propertyName);
-
-        if (elementValues.Any())
-        {
-            // Create a list to hold the property values.
-            List<string> propertyValues = new List<string>();
-
-            foreach (var element in elementValues)
-            {
-                propertyValues.Add(element.Value);
-            }
-
-            // Create a new entity and insert it into the table
-            var entity = new DynamicTableEntity(
-                partitionKey: blobName,
-                rowKey: "IndustryCode");
-
-            string jsonPropertyValue = JsonSerializer.Serialize(propertyValues);
-
-            entity.Properties[propertyName] = new EntityProperty(jsonPropertyValue);
-
-            // InsertOrMerge the table entity into the table.
-            TableOperation insertOperation = TableOperation.InsertOrMerge(entity);
-            await table.ExecuteAsync(insertOperation);
-        }
+        return;
     }
-}
 
+    foreach (var entityInfo in entityInformations)
+    {
+        var industryCode = entityInfo.Descendants(name: "IndustryCode").FirstOrDefault()?.Value;
+
+        if (industryCode is null)
+            return;
+
+        // Step 1 : Create a new entity and insert it into the table
+        var entity = new DynamicTableEntity(
+            partitionKey: partitionKey,
+            rowKey: industryCode);
+
+        foreach (var propertyName in propertyNames)
+        {
+            entity.Properties[propertyName] = new EntityProperty(entityInfo.Descendants(name: propertyName).FirstOrDefault()?.Value);
+        }
+
+        // InsertOrMerge the table entity into the table.
+        TableOperation insertOperation = TableOperation.InsertOrMerge(entity);
+        await table.ExecuteAsync(insertOperation);
+    }
+
+}
